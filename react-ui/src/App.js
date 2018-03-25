c2a5ac6e65f27c1eae4ca08ae88a991b527123fb
kevin_kim <kevkim1120@gmail.com>
2018-03-25 13:42:58 -0700

    added react bootstrap and changed the tables to bootstrap tables. Looks great 👍

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 // import Immutable from 'immutable'
-import 'react-virtualized/styles.css';
+import { Table } from 'react-bootstrap';
 import './css/App.css';
-import MyTable from './components/spotifyTable.js'
+
+/** Utility Functions*/
+function createArtistsName(item){
+  /** Helper Function for getUsersTopTracks */
+  let artistsName = "";
+  if(item['artists'].length > 1) {
+    item['artists'].forEach((artist)=>{
+      artistsName += (artist.name + ', ')
+    });
+    artistsName = artistsName.replace(/,\s*$/, "");
+  } else {
+    artistsName = item['artists'][0].name
+  }
+  return artistsName
+}
+
+function createSongName(item){
+  /** Helper Function for getUsersTopTracks*/
+  return item.name
+}
+
+function createPlayCount(item) {
+  return item.timestamps.length
+}
 
 class App extends Component {
   // static childContextTypes = {
@@ -22,8 +45,6 @@ class App extends Component {
       fetching: true,
     };
   }
-
-
 
   componentDidMount() {
     this.getPlayedSong()
@@ -85,14 +106,42 @@ class App extends Component {
     if(!this.state.data){return <p> {'LOADING'} </p>}
     const {data} = this.state;
 
+    const tableHead = (
+      <thead>
+      <tr>
+        <th>index</th>
+        <th>Name</th>
+        <th>Artists</th>
+        <th>Play Count</th>
+      </tr>
+      </thead>
+    );
+
+    const tableBody = data.map((item, i) => {
+      let artistsName = createArtistsName(item);
+      let songName = createSongName(item);
+      let playCount = createPlayCount(item);
+
+      return (
+        <tr id={i} key={i}>
+          <td>{i+1}</td>
+          <td>{songName}</td>
+          <td>{artistsName}</td>
+          <td>{playCount}</td>
+        </tr>
+      )
+    });
+
+
     return (
-      <MyTable list={data}/>
+    <Table striped bordered condensed hover>
+      {tableHead}
+      <tbody>
+      {tableBody}
+      </tbody>
+    </Table>
     );
   }
 }
-
-// App.childContextTypes = {
-//   list: PropTypes.instanceOf(Immutable.list)
-// };
 
 export default App;
